2ab574a5216f1348ffc7d8142233246f8fc55542
your_name <your_email>
2018-12-12 16:24:06 +0800

    test

---
+++ myblog/front/precache-manifest.89dc441c57ba2cd74f013deab1dbc80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "703f4331b952949e410a",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "703f4331b952949e410a",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "276e2aa4f9956892238e623b31ca8e75",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "79ef0f3f7611e6243e7a3ff52f9fe4bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1cc9049534a8a19a200da8a12ad273b",
+    "url": "/icons/icon.jpeg"
+  }
+];
